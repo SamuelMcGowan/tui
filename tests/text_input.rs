0df7c1920e92f72c91505d3b1f051989f1fbda53
squoop <the.sam.mcg@protonmail.com>
2023-05-08 22:22:07 +0100

    Fix broken test.

--- tests/text_input.rs
+++ tests/text_input.rs
@@ -1,15 +1,14 @@
 use std::time::Duration;
 
 use tui::app::App;
-use tui::widget::Context;
+use tui::callback::Callback;
 use tui::widgets::{TextInput, TextInputState};
 
 #[test]
 fn foo() {
-    let root =
-        TextInput::new().on_enter(|_ctx: &mut Context<_, _>, widget: &mut TextInputState| {
-            widget.text.clear();
-        });
+    let root = TextInput::new().on_enter(Callback::new(|_ctx, widget: &mut TextInputState| {
+        widget.text.clear();
+    }));
 
     let app: App<(), ()> = App::new((), root, Duration::from_millis(16)).unwrap();
     app.run().unwrap();
