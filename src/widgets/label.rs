ba9866d50280331ba600a16fcba40512c0b3b86e
squoop <the.sam.mcg@protonmail.com>
2023-05-08 17:55:42 +0100

    Add label widget.

---
+++ src/widgets/label.rs
@@ -0,0 +1,37 @@
+use crate::buffer::{BufferView, Cell};
+use crate::style::Style;
+use crate::widget::Widget;
+
+#[derive(Default)]
+pub struct Label {
+    text: String,
+    style: Style,
+}
+
+impl Label {
+    pub fn new(text: impl Into<String>) -> Self {
+        Self {
+            text: text.into(),
+            style: Style::default(),
+        }
+    }
+
+    pub fn with_style(mut self, style: Style) -> Self {
+        self.style = style;
+        self
+    }
+}
+
+impl<State, Msg> Widget<State, Msg> for Label {
+    fn render(&mut self, buf: &mut BufferView) {
+        let size = buf.size();
+
+        if size.y == 0 {
+            return;
+        }
+
+        for (x, c) in self.text.chars().enumerate().take(size.x as usize) {
+            buf[[x as u16, 0]] = Some(Cell::new(c, self.style));
+        }
+    }
+}
--- src/widgets/mod.rs
+++ src/widgets/mod.rs
@@ -1,9 +1,11 @@
 mod hooked;
+mod label;
 mod stack;
 mod string_editor;
 mod text_input;
 
 pub use hooked::Hooked;
+pub use label::Label;
 pub use stack::*;
 pub use string_editor::StringEditor;
 pub use text_input::{TextInput, TextInputState};
