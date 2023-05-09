aad7932f7c875351fd130d399a133ac7fb2b0e35
squoop <the.sam.mcg@protonmail.com>
2023-05-09 18:17:56 +0100

    Only rebuild view when requested.

--- src/widget2/app.rs
+++ src/widget2/app.rs
@@ -1,8 +1,7 @@
-use std::collections::VecDeque;
 use std::io;
 use std::time::{Duration, Instant};
 
-use super::widget::{View, Widget};
+use super::widget::{View, Widget, WidgetWithView};
 use crate::buffer::Buffer;
 use crate::draw_buffer::draw_diff;
 use crate::platform::event::Events;
@@ -15,89 +14,128 @@ pub enum ShouldQuit {
     No,
 }
 
-pub struct App<W: Widget> {
-    root: W,
+pub struct App<'a, W: Widget> {
+    root: Option<WidgetWithView<'a, W>>,
 
-    root_buf_prev: Buffer,
-    root_buf: Buffer,
+    buf_old: Buffer,
+    buf_new: Buffer,
 
     term: LinuxTerminal,
 
     context: Context<W::Msg>,
+    messages_current: Vec<W::Msg>, // A buffer for messages currently being processed.
 
     refresh_rate: Duration,
 }
 
-impl<W: Widget> App<W> {
+impl<'a, W: Widget> App<'a, W> {
+    pub fn new(widget: &'a mut W) -> io::Result<Self> {
+        Ok(Self {
+            root: Some(widget.build()),
+
+            buf_old: Buffer::default(),
+            buf_new: Buffer::default(),
+
+            term: LinuxTerminal::init()?,
+
+            context: Context {
+                messages: vec![],
+                should_rebuild_view: false,
+                should_quit: false,
+            },
+            messages_current: vec![],
+
+            refresh_rate: Duration::from_millis(17),
+        })
+    }
+
     pub fn run(mut self) -> io::Result<()> {
-        while let ShouldQuit::No = self.frame()? {}
+        loop {
+            self.frame()?;
+
+            if self.context.should_quit {
+                break;
+            }
+
+            if self.context.should_rebuild_view {
+                self.rebuild_view();
+            }
+
+            self.render()?;
+        }
+
         Ok(())
     }
 
-    pub fn frame(&mut self) -> io::Result<ShouldQuit> {
+    fn rebuild_view(&mut self) {
+        let root_widget = self.root.take().unwrap().widget;
+        self.root = Some(root_widget.build());
+    }
+
+    fn frame(&mut self) -> io::Result<()> {
         // Work out how long we have.
         let time = Instant::now();
         let deadline = time
             .checked_add(self.refresh_rate)
             .expect("deadline overflowed");
 
-        self.root.update();
+        let root = self.root.as_mut().unwrap();
 
-        // Build widget view.
-        let mut widget_with_view = self.root.build();
+        // Update the widget tree.
+        root.widget.update();
 
-        // Process events and propagate them through the view tree.
         let events = self.term.events();
         while let Some(event) = events.read_with_deadline(deadline)? {
-            let _ = widget_with_view.propagate_event(&mut self.context, &event);
+            let _ = root.propagate_event(&mut self.context, &event);
         }
 
-        // Propagate messages.
-        let messages = std::mem::take(&mut self.context.messages);
-        for message in messages {
-            let _ = widget_with_view.widget.propagate_msg(&mut self.context, message);
+        std::mem::swap(&mut self.messages_current, &mut self.context.messages);
+        for message in self.messages_current.drain(..) {
+            let _ = root.widget.propagate_msg(&mut self.context, message);
         }
 
-        render(
-            &mut self.root_buf_prev,
-            &mut self.root_buf,
-            widget_with_view,
-            &mut self.term,
-        )?;
+        Ok(())
+    }
 
-        Ok(match self.context.quit {
-            true => ShouldQuit::Yes,
-            false => ShouldQuit::No,
-        })
+    fn render(&mut self) -> io::Result<()> {
+        let root = self.root.as_mut().unwrap();
+
+        // Resize buffer.
+        let term_size = self.term.size()?;
+        self.buf_new.resize_and_clear(term_size);
+
+        // Render widget to buffer.
+        let mut buf_view = self.buf_new.view(true);
+        root.render(&mut buf_view);
+
+        // Draw changes to terminal.
+        // TODO: make immutable view type.
+        let buf_old_view = self.buf_old.view(false);
+        draw_diff(&buf_old_view, &buf_view, self.term.writer());
+
+        // Swap buffers.
+        self.buf_old.clone_from(&self.buf_new);
+
+        self.term.writer().flush()
     }
 }
 
-fn render<Msg>(
-    buf_old: &mut Buffer,
-    buf_new: &mut Buffer,
-    view: impl View<Msg>,
-    term: &mut LinuxTerminal,
-) -> io::Result<()> {
-    // Resize buffer.
-    let term_size = term.size()?;
-    buf_new.resize_and_clear(term_size);
-
-    // Render widget to buffer.
-    let mut buf_view = buf_new.view(true);
-    view.render(&mut buf_view);
-
-    // Draw changes to terminal.
-    // TODO: make immutable view type.
-    let buf_old_view = buf_old.view(false);
-    draw_diff(&buf_old_view, &buf_view, term.writer());
-
-    // Swap buffers.
-    buf_old.clone_from(buf_new);
-
-    term.writer().flush()
+pub struct Context<Msg> {
+    messages: Vec<Msg>,
+    should_rebuild_view: bool,
+    should_quit: bool,
 }
 
-pub struct Context<Msg> {
-    messages: VecDeque<Msg>,
-    quit: bool,
+impl<Msg> Context<Msg> {
+    pub fn send(&mut self, message: Msg) {
+        self.messages.push(message);
+    }
+
+    pub fn quit(&mut self) {
+        self.should_quit = true;
+    }
+
+    pub fn rebuild_view(&mut self) {
+        self.should_rebuild_view = true;
+    }
 }
